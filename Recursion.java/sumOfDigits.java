3b71a58d6d5bbb7f496a748541fb0d228cb2acc7
Siddharth Singh <siddhar4838@gmail.com>
2026-05-23 16:25:59 +0530

    Update sumOfDigits.java

--- Recursion.java/sumOfDigits.java
+++ Recursion.java/sumOfDigits.java
@@ -1,10 +1,12 @@
 class Solution {
     static int sumOfDigits(int n) {
         // code here
+        // base case
         if(n == 0) {
             return 0;
         }
-        
+
+        // small problem
         int digit = n%10;
         int res = sumOfDigits(n/10);
         return res + digit;
